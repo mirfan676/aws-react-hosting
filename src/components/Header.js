66ee29477c1181b6014f10415ee10f3591b3f6a1
Muhammad Irfan <88087250+mirfan676@users.noreply.github.com>
2023-01-04 18:55:09 +0500

    few changes in header
    header second portion with few html and css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink, Link, useSearchParams } from 'react-router-dom'
-import {BsSearch} from 'react-icons/bs';
+import { BsSearch } from 'react-icons/bs';
 const Header = () => {
   return (
     <>
@@ -17,7 +17,7 @@ const Header = () => {
               <p className='text-end text-white mb-0'>
                 Hotline:
                 <a className='text-white'
-                href='tel:+923066762289'>
+                  href='tel:+923066762289'>
                   +92 306 676 2289
                 </a>
               </p>
@@ -28,21 +28,25 @@ const Header = () => {
       </header>
       <header className='header-upper py-3'>
         <div className='container-xxl'>
-          <div className='row'>
+          <div className='row align-items-center'>
             <div className='col-3'>
               <h1>
                 <Link className='text-white'>Saeed&Sons</Link>
               </h1>
             </div>
             <div className='col-5'>
-            <div class="input-group mb-3">
-              <input type="text" className='form-control'
-              placeholder="Search Products Here"
-              aria-label="Search Products Here"
-              aria-describedby="basic-addon2"/>
-              <span className='input-group-text'
-              id="basic-addon2"><BsSearch/></span>
-            </div>
+              <div class="input-group mb-3">
+                <input type="text"
+                  className='form-control py-2'
+                  placeholder="Search Products Here"
+                  aria-label="Search Products Here"
+                  aria-describedby="basic-addon2" />
+
+                <span className='input-group-text p-3'
+                  id="basic-addon2"><BsSearch className='fs-6' />
+                </span>
+
+              </div>
 
             </div>
             <div className='col-4'>
